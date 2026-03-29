cbcbcb618446d1164bb03704b3ea2cc4ac33086c
Baggio67 <michelbaggio172@gmail.com>
2026-03-29 11:04:08 +0300

    Adjust Gradle root configuration and verify build command execution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
-plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.24-1.0.20")
+    }
 }
